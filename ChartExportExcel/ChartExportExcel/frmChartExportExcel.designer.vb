26ccb7122c3f2cb4750e1b9fad904778bec1b9bc
Thongkorn Tubtimkrob <thongkorn@hotmail.com>
2021-09-25 14:51:06 +0700

    Add files via upload

---
+++ ChartExportExcel/ChartExportExcel/frmChartExportExcel.designer.vb
@@ -0,0 +1,228 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmChartExportExcel
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmChartExportExcel))
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.dgvData = New System.Windows.Forms.DataGridView()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.btnGenData = New System.Windows.Forms.Button()
+        Me.btnChart = New System.Windows.Forms.Button()
+        Me.btnExportExcel = New System.Windows.Forms.Button()
+        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.cmbChartType = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.chkShowLegend = New System.Windows.Forms.CheckBox()
+        Me.btnCopyClipboard = New System.Windows.Forms.Button()
+        CType(Me.dgvData, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(2, 58)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 14)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Label1"
+        '
+        'dgvData
+        '
+        Me.dgvData.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvData.Location = New System.Drawing.Point(1, 74)
+        Me.dgvData.Margin = New System.Windows.Forms.Padding(2)
+        Me.dgvData.Name = "dgvData"
+        Me.dgvData.RowTemplate.Height = 24
+        Me.dgvData.Size = New System.Drawing.Size(1052, 241)
+        Me.dgvData.TabIndex = 7
+        '
+        'btnClose
+        '
+        Me.btnClose.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Moccasin
+        Me.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnClose.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnClose.Image = CType(resources.GetObject("btnClose.Image"), System.Drawing.Image)
+        Me.btnClose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnClose.Location = New System.Drawing.Point(527, 12)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(124, 31)
+        Me.btnClose.TabIndex = 4
+        Me.btnClose.Text = "Close Program"
+        Me.btnClose.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'btnGenData
+        '
+        Me.btnGenData.BackColor = System.Drawing.SystemColors.Control
+        Me.btnGenData.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnGenData.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Moccasin
+        Me.btnGenData.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnGenData.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnGenData.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.btnGenData.Image = CType(resources.GetObject("btnGenData.Image"), System.Drawing.Image)
+        Me.btnGenData.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnGenData.Location = New System.Drawing.Point(6, 13)
+        Me.btnGenData.Name = "btnGenData"
+        Me.btnGenData.Size = New System.Drawing.Size(124, 31)
+        Me.btnGenData.TabIndex = 0
+        Me.btnGenData.Text = "Generate Data"
+        Me.btnGenData.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnGenData.UseVisualStyleBackColor = False
+        '
+        'btnChart
+        '
+        Me.btnChart.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnChart.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Moccasin
+        Me.btnChart.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnChart.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnChart.Image = CType(resources.GetObject("btnChart.Image"), System.Drawing.Image)
+        Me.btnChart.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnChart.Location = New System.Drawing.Point(136, 13)
+        Me.btnChart.Name = "btnChart"
+        Me.btnChart.Size = New System.Drawing.Size(124, 31)
+        Me.btnChart.TabIndex = 1
+        Me.btnChart.Text = "Chart Sample"
+        Me.btnChart.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnChart.UseVisualStyleBackColor = True
+        '
+        'btnExportExcel
+        '
+        Me.btnExportExcel.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnExportExcel.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Moccasin
+        Me.btnExportExcel.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnExportExcel.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnExportExcel.Image = CType(resources.GetObject("btnExportExcel.Image"), System.Drawing.Image)
+        Me.btnExportExcel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnExportExcel.Location = New System.Drawing.Point(267, 13)
+        Me.btnExportExcel.Name = "btnExportExcel"
+        Me.btnExportExcel.Size = New System.Drawing.Size(124, 31)
+        Me.btnExportExcel.TabIndex = 2
+        Me.btnExportExcel.Text = "Export Excel"
+        Me.btnExportExcel.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnExportExcel.UseVisualStyleBackColor = True
+        '
+        'Chart1
+        '
+        Me.Chart1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        ChartArea1.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea1)
+        Me.Chart1.Location = New System.Drawing.Point(1, 320)
+        Me.Chart1.Name = "Chart1"
+        Me.Chart1.Size = New System.Drawing.Size(1052, 318)
+        Me.Chart1.TabIndex = 8
+        Me.Chart1.Text = "Chart1"
+        '
+        'cmbChartType
+        '
+        Me.cmbChartType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbChartType.FormattingEnabled = True
+        Me.cmbChartType.Location = New System.Drawing.Point(737, 17)
+        Me.cmbChartType.Name = "cmbChartType"
+        Me.cmbChartType.Size = New System.Drawing.Size(140, 22)
+        Me.cmbChartType.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(663, 20)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(68, 14)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Chart Type"
+        '
+        'chkShowLegend
+        '
+        Me.chkShowLegend.AutoSize = True
+        Me.chkShowLegend.Checked = True
+        Me.chkShowLegend.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.chkShowLegend.Location = New System.Drawing.Point(737, 50)
+        Me.chkShowLegend.Name = "chkShowLegend"
+        Me.chkShowLegend.Size = New System.Drawing.Size(107, 18)
+        Me.chkShowLegend.TabIndex = 6
+        Me.chkShowLegend.Text = "Show Legends"
+        Me.chkShowLegend.UseVisualStyleBackColor = True
+        '
+        'btnCopyClipboard
+        '
+        Me.btnCopyClipboard.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnCopyClipboard.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Moccasin
+        Me.btnCopyClipboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnCopyClipboard.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnCopyClipboard.Image = CType(resources.GetObject("btnCopyClipboard.Image"), System.Drawing.Image)
+        Me.btnCopyClipboard.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnCopyClipboard.Location = New System.Drawing.Point(397, 12)
+        Me.btnCopyClipboard.Name = "btnCopyClipboard"
+        Me.btnCopyClipboard.Size = New System.Drawing.Size(124, 31)
+        Me.btnCopyClipboard.TabIndex = 3
+        Me.btnCopyClipboard.Text = "Copy to Clipboard"
+        Me.btnCopyClipboard.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnCopyClipboard.UseVisualStyleBackColor = True
+        '
+        'frmChartExportExcel
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 14.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1054, 639)
+        Me.Controls.Add(Me.btnCopyClipboard)
+        Me.Controls.Add(Me.chkShowLegend)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.cmbChartType)
+        Me.Controls.Add(Me.btnExportExcel)
+        Me.Controls.Add(Me.Chart1)
+        Me.Controls.Add(Me.btnChart)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnGenData)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.dgvData)
+        Me.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.KeyPreview = True
+        Me.Name = "frmChartExportExcel"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Sample Chart & Export Data From DataGridView to Excel - coDe bY: Thongkorn Tubtim" & _
+    "krob"
+        CType(Me.dgvData, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Private WithEvents dgvData As System.Windows.Forms.DataGridView
+    Friend WithEvents btnClose As System.Windows.Forms.Button
+    Friend WithEvents btnGenData As System.Windows.Forms.Button
+    Friend WithEvents btnChart As System.Windows.Forms.Button
+    Friend WithEvents btnExportExcel As System.Windows.Forms.Button
+    Friend WithEvents Chart1 As System.Windows.Forms.DataVisualization.Charting.Chart
+    Friend WithEvents cmbChartType As System.Windows.Forms.ComboBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents chkShowLegend As System.Windows.Forms.CheckBox
+    Friend WithEvents btnCopyClipboard As System.Windows.Forms.Button
+
+End Class
---
+++ ChartExportExcel/ChartExportExcel/frmChartExportExcel.vb
@@ -0,0 +1,275 @@
+﻿#Region "ABOUT"
+' / --------------------------------------------------------------------------
+' / Developer : Mr.Surapon Yodsanga (Thongkorn Tubtimkrob)
+' / eMail : thongkorn@hotmail.com
+' / URL: http://www.g2gnet.com (Khon Kaen - Thailand)
+' / Facebook: https://www.facebook.com/g2gnet (For Thailand)
+' / Facebook: https://www.facebook.com/commonindy (Worldwide)
+' / More Info: http://www.g2gnet.com/webboard
+' /
+' / Purpose: Create data into DataGridView for make the Chart and Export data to Excel @Run Time.
+' / Microsoft Visual Basic .NET (2010) + MS Excel 14.0
+' /
+' / This is open source code under @CopyLeft by Thongkorn Tubtimkrob.
+' / You can modify and/or distribute without to inform the developer.
+' / --------------------------------------------------------------------------
+#End Region
+
+Imports System.Windows.Forms.DataVisualization.Charting
+Imports Excel = Microsoft.Office.Interop.Excel
+Imports System.IO
+
+Public Class frmChartExportExcel
+
+    Private Sub frmPlotGraph_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Label1.Text = ""
+        '// Chart Type
+        '// https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.datavisualization.charting.seriescharttype?view=netframework-4.7.2
+        cmbChartType.Items.Add("Column")
+        cmbChartType.Items.Add("Line")
+        cmbChartType.Items.Add("Point")
+        cmbChartType.SelectedIndex = 0
+
+        '// Setup DataGridView
+        Call InitializeGrid()
+        '// Generate Data
+        Call btnGenData_Click(sender, e)
+        '//
+    End Sub
+
+    Private Sub btnGenData_Click(sender As System.Object, e As System.EventArgs) Handles btnGenData.Click
+        Call FillData()
+        Call btnChart_Click(sender, e)
+    End Sub
+
+    ' / --------------------------------------------------------------------------
+    Private Sub FillData()
+        Dim dt As New DataTable
+        dt.Columns.Add("Item")
+        dt.Columns.Add("Value1")
+        dt.Columns.Add("Value2")
+        dt.Columns.Add("Value3")
+        dt.Columns.Add("Average")
+        Dim RandomClass As New Random()
+        Dim MyMonth As String() = New String() {("ม.ค."), ("ก.พ."), ("มี.ค."), ("เม.ย."), ("พ.ค."), ("มิ.ย."), ("ก.ค."), ("ส.ค."), ("ก.ย."), ("ต.ค."), ("พ.ย."), ("ธ.ค.")}
+        For i As Byte = 0 To 11
+            Dim dr As DataRow = dt.NewRow()
+            dr(0) = MyMonth(i)
+            dr(1) = FormatNumber(RandomClass.Next(100, 1000) + RandomClass.NextDouble(), 2)
+            dr(2) = FormatNumber(RandomClass.Next(100, 1000) + RandomClass.NextDouble(), 2)
+            dr(3) = FormatNumber(RandomClass.Next(100, 1000) + RandomClass.NextDouble(), 2)
+            '// CDbl = Convert to Double
+            dr(4) = Format((CDbl(dr(1)) + CDbl(dr(2)) + CDbl(dr(3))) / 3, "#,##0.00")
+            dt.Rows.Add(dr)
+        Next
+        dgvData.DataSource = dt
+        Label1.Text = "Total : " & dt.Rows.Count.ToString("#,##") & " Records."
+    End Sub
+
+    ' / --------------------------------------------------------------------------------
+    '// การตั้งค่าเริ่มต้นให้กับตารางกริดในแบบ @Run Time
+    Private Sub InitializeGrid()
+        With dgvData
+            .RowHeadersVisible = False
+            .AllowUserToAddRows = False
+            .AllowUserToDeleteRows = False
+            .AllowUserToResizeRows = False
+            .MultiSelect = False
+            .SelectionMode = DataGridViewSelectionMode.FullRowSelect
+            .ReadOnly = True
+            .Font = New Font("Tahoma", 9)
+            '/ จัดความกว้างของแต่ละหลัก
+            .AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+            .AutoResizeColumns()
+            '/ Adjust Header Styles
+            With .ColumnHeadersDefaultCellStyle
+                .BackColor = Color.Navy
+                .ForeColor = Color.White
+                .Font = New Font("Tahoma", 9, FontStyle.Bold)
+            End With
+        End With
+    End Sub
+
+    ' / --------------------------------------------------------------------------------
+    Private Sub btnChart_Click(sender As System.Object, e As System.EventArgs) Handles btnChart.Click
+        If dgvData.RowCount = 0 Then Return
+        '//
+        Chart1.Titles.Clear()
+        '// Clear Legends & Series
+        Chart1.Series.Clear()
+        Chart1.Legends.Clear()
+        '// Clear Points
+        For Each xSeries In Chart1.Series
+            xSeries.Points.Clear()
+        Next
+        '// Create Legends
+        Dim Legend1 As Legend = New Legend()
+        Legend1.Name = "Legend1"
+        Chart1.Legends.Add(Legend1)
+        '// Create Series
+        Dim Series1 As Series = New Series()
+        Dim Series2 As Series = New Series()
+        Dim Series3 As Series = New Series()
+        Dim Series4 As Series = New Series()
+        '// Add Series
+        With Chart1
+            .Series.Add(Series1)
+            .Series.Add(Series2)
+            .Series.Add(Series3)
+            .Series.Add(Series4)
+        End With
+        '//
+        Try
+            Dim myFont As New Font("Tahoma", 14, FontStyle.Bold)
+            Chart1.Titles.Add("Sample Chart")
+            Chart1.Titles(0).Font = myFont
+            '// Define Chart Type.
+            For Each xSeries In Chart1.Series
+                Select Case cmbChartType.Text
+                    Case "Column"
+                        xSeries.ChartType = SeriesChartType.Column
+                    Case "Line"
+                        xSeries.ChartType = SeriesChartType.Line
+                    Case "Point"
+                        xSeries.ChartType = SeriesChartType.Point
+                End Select
+                '// Show Legend
+                If chkShowLegend.Checked Then
+                    xSeries.IsVisibleInLegend = True
+                Else
+                    xSeries.IsVisibleInLegend = False
+                End If
+                '// View the value of a chart point on mouse over.
+                xSeries.ToolTip = "#VAL{0.00}"
+            Next
+            '//
+            With Chart1
+                .Series(0).LegendText = "Value 1"
+                .Series(1).LegendText = "Value 2"
+                .Series(2).LegendText = "Value 3"
+                .Series(3).LegendText = "Average"
+            End With
+            '// Show all Axis Label
+            Chart1.ChartAreas(0).AxisX.Interval = 1
+            '//
+            With Chart1.ChartAreas("ChartArea1")
+                .AxisX.MajorGrid.LineWidth = 1
+                .AxisY.MajorGrid.LineWidth = 1
+            End With
+
+            '// LOOP DISPLAY
+            For CountData As Integer = 0 To dgvData.Rows.Count - 1
+                With Chart1
+                    '// 0 = Month, 1 = Value1, 2 = Value2, 3 = Value3, 4 = Average
+                    .Series(0).Points.AddXY(dgvData.Item(0, CountData).Value, dgvData.Item(1, CountData).Value)
+                    .Series(1).Points.AddXY(dgvData.Item(0, CountData).Value, dgvData.Item(2, CountData).Value)
+                    .Series(2).Points.AddXY(dgvData.Item(0, CountData).Value, dgvData.Item(3, CountData).Value)
+                    .Series(3).Points.AddXY(dgvData.Item(0, CountData).Value, dgvData.Item(4, CountData).Value)
+                End With
+            Next
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    ' / --------------------------------------------------------------------------------
+    '// Export data to Excel.
+    Private Sub btnExportExcel_Click(sender As System.Object, e As System.EventArgs) Handles btnExportExcel.Click
+        '// ไม่มีข้อมูลในตารางกริด ก็สั่งให้เด้งหนีออกไป
+        If dgvData.Rows.Count = 0 Then Exit Sub
+        '// พิจารณาการเลือกใช้ชนิดข้อมูล (Data Type) ให้เหมาะสม
+        Dim MaxRow As Integer, MaxCol As Short
+        Dim nRow As Integer, nCol As Short
+        Dim xlsApp As New Excel.Application
+        Dim xlsWorkBook As Excel.Workbook = xlsApp.Workbooks.Add
+        Dim xlsWorkSheet As Excel.Worksheet = CType(xlsWorkBook.Worksheets(1), Excel.Worksheet)
+        '// S T A R T
+        Try
+            xlsApp.Visible = True
+            '// หาค่าจำนวนแถว
+            MaxRow = dgvData.RowCount
+            '// หาค่าจำนวนหลัก
+            MaxCol = dgvData.Columns.Count - 1
+            With xlsWorkSheet
+                .Cells.Select()
+                .Cells.Delete()
+                '// Header
+                For nCol = 0 To MaxCol
+                    .Cells(1, nCol + 1).Value = dgvData.Columns(nCol).HeaderText
+                Next nCol
+                '// ไล่ตามจำนวนแถว
+                For nRow = 0 To MaxRow - 1
+                    For nCol = 0 To MaxCol
+                        .Cells(nRow + 2, nCol + 1).value = dgvData.Rows(nRow).Cells(nCol).Value
+                    Next nCol   '// Nested Loop
+                    '// หากชุดคำสั่งที่อยู่ใน Nested Loop 
+                    '// การให้ตัวแปรต่อท้าย Next จะช่วยให้เรารู้ว่ามันอยู่ใน Loop ไหน
+                Next nRow
+
+                '// กำหนดรูปแบบใน WorkSheet
+                .Rows("1:1").Font.FontStyle = "Bold"
+                .Rows("1:1").Font.Size = 10
+                '//
+                .Cells.Columns.AutoFit()
+                .Cells.Select()
+                .Cells.EntireColumn.AutoFit()
+                .Cells(1, 1).Select()
+            End With
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+        xlsApp = Nothing
+        '// Release memory.
+        ReleaseObject(xlsApp)
+        ReleaseObject(xlsWorkBook)
+        ReleaseObject(xlsWorkSheet)
+    End Sub
+
+    ' / ------------------------------------------------------------------
+    '// คืนหน่วยความจำกลับคืนสู่ระบบ
+    Public Sub ReleaseObject(ByVal obj As Object)
+        Try
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(obj)
+            obj = Nothing
+        Catch ex As Exception
+            obj = Nothing
+        Finally
+            '// GC = Garbage Collection
+            GC.Collect()
+        End Try
+    End Sub
+
+    Private Sub frmPlotGraph_FormClosed(sender As Object, e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+        Me.Dispose()
+        GC.SuppressFinalize(Me)
+        Application.Exit()
+    End Sub
+
+    Private Sub btnClose_Click(sender As System.Object, e As System.EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+
+    '// เวลากด Enter ในแถวของตารางกริด จะไม่เกิดการเลื่อนตำแหน่ง
+    Private Sub dgvData_KeyDown(sender As Object, e As System.Windows.Forms.KeyEventArgs) Handles dgvData.KeyDown
+        If e.KeyCode = Keys.Enter Then e.SuppressKeyPress = True
+    End Sub
+
+    Private Sub cmbChartType_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles cmbChartType.SelectedIndexChanged
+        Call btnChart_Click(sender, e)
+    End Sub
+
+    Private Sub chkShowLegend_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles chkShowLegend.CheckedChanged
+        Call btnChart_Click(sender, e)
+    End Sub
+
+    '// Copy to Clipboard
+    Private Sub btnCopyClipboard_Click(sender As System.Object, e As System.EventArgs) Handles btnCopyClipboard.Click
+        Using ms As MemoryStream = New MemoryStream()
+            Chart1.SaveImage(ms, ChartImageFormat.Bmp)
+            Dim bm As Bitmap = New Bitmap(ms)
+            Clipboard.SetImage(bm)
+        End Using
+    End Sub
+End Class
+
